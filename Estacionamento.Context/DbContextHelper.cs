05a1f7d3ecec1e3def6d0719ee6b4fc118cd087a
luizpine <pine998@hotmail.com>
2023-04-14 07:37:20 -0300

    configurações de banco

---
+++ Estacionamento.Context/DbContextHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Estacionamento.Context;
+
+public static class DbContextHelper
+{
+
+    public static void RemovePluralizingTableNameConvention(this ModelBuilder modelBuilder)
+    {
+        foreach (var entity in modelBuilder.Model.GetEntityTypes())
+        {
+            if (entity.ClrType.Name.Contains("Dictionary"))
+                continue;
+
+            var tableName = entity.DisplayName();
+            entity.SetTableName(tableName);
+        }
+    }
+
+}
--- Estacionamento.Context/EstacionamentoContext.cs
+++ Estacionamento.Context/EstacionamentoContext.cs
@@ -1,5 +1,7 @@
-﻿using Estacionamento.Domain.Clientes;
+﻿using Estacionamento.Context;
+using Estacionamento.Domain.Clientes;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace Estacionamento.Domain.Context;
 
@@ -7,10 +9,17 @@ public class EstacionamentoContext : DbContext, IEstacionamentoContext
 {
     public EstacionamentoContext(DbContextOptions<EstacionamentoContext> options) : base(options)
     {
-
     }
 
     public DbSet<Cliente> Clientes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        modelBuilder.RemovePluralizingTableNameConvention();
+    }
 
     public override int SaveChanges()
     {
