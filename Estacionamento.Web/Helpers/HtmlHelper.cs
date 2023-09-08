b3a20d796778c7ceb49f9a36a043362ba23aa513
luizpine <pine998@hotmail.com>
2023-09-07 21:35:12 -0300

    .

--- Estacionamento.Web/Helpers/HtmlHelper.cs
+++ Estacionamento.Web/Helpers/HtmlHelper.cs
@@ -6,13 +6,17 @@ namespace Estacionamento.Web.Helpers
 {
     public static class HtmlHelper
     {
-        public static IHtmlContent Pagination<T>(this IHtmlHelper helper, ResponsePagination<T> pagination) where T : class
+        public static IHtmlContent Pagination<T>(this IHtmlHelper helper, ResponsePagination<T> pagination,string controller) where T : class
         {
             var content = new HtmlContentBuilder()
                     .AppendHtml("<ol class='content-body'><li>")
                     .AppendHtml(helper.ActionLink("Home", "Index", "Home"))
                     .AppendHtml("</li>");
-
+            /*
+            content.AppendHtml("<ol class='content-body'><li  class=\"page-item\">")
+                .AppendHtml(helper.ActionLink("Prev", "Index", controller, new {@class = "page-link" }))
+                .AppendHtml("</li>");
+            */
 
             content.AppendHtml("O total da listagem é :" + pagination.Total);
             content.AppendHtml("A pagina atual é :" + pagination.CurrentPage);
