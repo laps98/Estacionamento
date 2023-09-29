1433ae04038c6ba11095dabbd29245d0977a8b5f
Luiz <pine998@hotmail.com>
2023-09-29 08:40:24 -0300

    .

--- Estacionamento.Web/Helpers/HtmlHelper.cs
+++ Estacionamento.Web/Helpers/HtmlHelper.cs
@@ -10,22 +10,33 @@ namespace Estacionamento.Web.Helpers
         {
             var content = new HtmlContentBuilder()
                     .AppendHtml("<nav>")
-                    .AppendHtml("<ul class=\"pagination\">")
+                    .AppendHtml("<ul class=\"pagination\">");
+            if (pagination.HasPreviousPage)
+                content.AppendHtml("<li class=\"page-item\">")
+                    .AppendHtml($"<a class=\"page-link\" href=\"?currentPage={pagination.CurrentPage - 1}&ItemsPerPage=10\">Anterior</a>")
+                    .AppendHtml("</li>");
+            content.AppendHtml("<li class=\"page-item\">")
+                .AppendHtml($"<a class=\"page-link\" href=\"?currentPage={pagination.CurrentPage}&ItemsPerPage=10\">{pagination.CurrentPage}</a>")
+                .AppendHtml("</li>");
+            if ((pagination.CurrentPage + 1) <= pagination.Total)
+                content.AppendHtml("<li class=\"page-item\">")
+                .AppendHtml($"<a class=\"page-link\" href=\"?currentPage={pagination.CurrentPage + 1}&ItemsPerPage=10\">{pagination.CurrentPage + 1}</a>")
+                .AppendHtml("</li>");
+            if ((pagination.CurrentPage + 2) <= pagination.Total)
+                content.AppendHtml("<li class=\"page-item\">")
+                .AppendHtml($"<a class=\"page-link\" href=\"?currentPage={pagination.CurrentPage + 2}&ItemsPerPage=10\">{pagination.CurrentPage + 2}</a>")
+                .AppendHtml("</li>");
+            if (pagination.Total > 5)
+                content.AppendHtml("<li class=\"page-item\">")
+                .AppendHtml($"<a class=\"page-link\" href=\"?currentPage={pagination.Total}&ItemsPerPage=10\">{pagination.Total}</a>")
+                .AppendHtml("</li>");
+            if (pagination.HasNextPage)
+                content.AppendHtml("<li class=\"page-item\">")
+                .AppendHtml($"<a class=\"page-link\" href=\"?currentPage={pagination.CurrentPage + 1}&ItemsPerPage=10\">Próximo</a>")
+                .AppendHtml("</li>");
+            content.AppendHtml("</ul>")
+            .AppendHtml("</nav>");
 
-                    .AppendHtml("<li class=\"page-item\">")
-                    .AppendHtml($"<a class=\"page-link\" href=\"?currentPage={pagination.CurrentPage - 1}&ItemsPerPage=10\">{pagination.CurrentPage}</a>")
-                    .AppendHtml("</li>")
-
-                    .AppendHtml("<li class=\"page-item\">")
-                    .AppendHtml($"<a class=\"page-link\" href=\"?currentPage={pagination.CurrentPage}&ItemsPerPage=10\">{pagination.CurrentPage}</a>")
-                    .AppendHtml("</li>")
-
-                    .AppendHtml("<li class=\"page-item\">")
-                    .AppendHtml($"<a class=\"page-link\" href=\"?currentPage={pagination.CurrentPage + 1}&ItemsPerPage=10\">{pagination.CurrentPage}</a>")
-                    .AppendHtml("</li>")
-
-                    .AppendHtml("</ul>")
-                    .AppendHtml("</nav>");
             /*
             content.AppendHtml("<ol class='content-body'><li  class=\"page-item\">")
                 .AppendHtml(helper.ActionLink("Prev", "Index", controller, new {@class = "page-link" }))
