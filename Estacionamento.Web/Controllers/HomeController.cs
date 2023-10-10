632d635f6a9a8981fc19d4d9b87ef7e37d11d297
Luiz <pine998@hotmail.com>
2023-10-10 14:17:42 -0300

    .

--- Estacionamento.Web/Controllers/HomeController.cs
+++ Estacionamento.Web/Controllers/HomeController.cs
@@ -1,12 +1,21 @@
-﻿using Estacionamento.Models;
+﻿using Estacionamento.Doamin.Helpers;
+using Estacionamento.Domain.Context;
+using Estacionamento.Domain.MovimentacoesDeVeiculo;
+using Estacionamento.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Diagnostics;
 
 namespace Estacionamento.Controllers;
 
 public class HomeController : Controller
 {
-    //private readonly ILogger<HomeController> _logger;
+    private readonly IEstacionamentoContext _context;
+
+    public HomeController(IEstacionamentoContext context)
+    {
+        _context = context;
+    }
 
     //public HomeController(ILogger<HomeController> logger)
     //{
@@ -15,9 +24,19 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        Dropdown();
+        var hoje = DateTime.Now;
+        var movimentacaoDeVeiculo = new MovimentacaoDeVeiculo
+        {
+            DataDeEntrada = DateTimeHelper.Atual(hoje)
+        };
+        return View(movimentacaoDeVeiculo);
     }
-
+    private void Dropdown(MovimentacaoDeVeiculo? movimentacaoDeVeiculo = null)
+    {
+        var items = _context.TabelasDePreco.Select(q => new { q.Id, q.Descricao });
+        ViewBag.TabelaDePreco = new SelectList(items, "Id", "Descricao", movimentacaoDeVeiculo?.IdTabelaDePreco);
+    }
     public IActionResult Privacy()
     {
         return View();
